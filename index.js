b043354c7408ab9ea3bc06aa643fd668d9ab263b
frodriguezIENTC <frodriguez@ientc.com>
2018-07-17 10:02:37 -0500

    JSX extension is added to the require statements in the index.js file.

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const { BasicTextBox: TextBox } = require('./src/textboxes/textboxes');
-const { Button } = require('./src/buttons/buttons');
+/*
+https://stackoverflow.com/questions/39436322/node-js-syntaxerror-unexpected-token-import
+
+While import is indeed part of ES6, it is unfortunately not yet supported in NodeJS by default, 
+and has only very recently landed support in browsers.
+
+From James M Snell's Update on ES6 Modules in Node.js (February 2017):
+
+| Work is in progress but it is going to take some time — We’re currently looking at around a year at least.
+
+Until support shows up natively, you'll have to continue using classic require statements:
+
+const express = require("express");
+*/
+
+const { BasicTextBox: TextBox } = require('./src/textboxes/textboxes.jsx');
+const { Button } = require('./src/buttons/buttons.jsx');
 
 module.exports = {
     TextBox,
