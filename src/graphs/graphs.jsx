bd1bc55cd62fdbcfc58e35f1f800d607e517698e
pecadorcelestial <unit_zero@hotmail.com>
2018-10-15 13:07:17 -0500

    Graphs: Removed the line breaks from the d property because of the stupid TravisCI tests failing.

--- src/graphs/graphs.jsx
+++ src/graphs/graphs.jsx
@@ -67,16 +67,12 @@ export class CircularGraph extends Component {
             <Layout {...this.props}>
                 <SVG viewBox="0 0 36 36">
                     <OutterPath
-                        d="M18 2.0845
-                        a 15.9155 15.9155 0 0 1 0 31.831
-                        a 15.9155 15.9155 0 0 1 0 -31.831"
+                        d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"
                     />
                     <InnerPath
                         percent={this.props.percent}
                         theme={this.props.theme}
-                        d="M18 2.0845
-                        a 15.9155 15.9155 0 0 1 0 31.831
-                        a 15.9155 15.9155 0 0 1 0 -31.831"
+                        d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"
                     />
                     <Text x="18" y="20.35">{`${this.props.percent}%`}</Text>
                 </SVG>
